09849a9443ed7f2b9660440fa9a23d827c0773ea
wolverineJo <noreply@michaelbarr.dev>
2023-05-31 16:27:56 -0700

    works without encoder

--- src/main/java/frc/robot/Devices/Encoder.java
+++ src/main/java/frc/robot/Devices/Encoder.java
@@ -1,15 +1,10 @@
 package frc.robot.Devices;
 
+import com.ctre.phoenix.sensors.CANCoder;
+
 public class Encoder {
-    edu.wpi.first.wpilibj.Encoder encoder;
-    boolean isReversed;
-    public Encoder(int port1, int port2, boolean isReversed) {
-        this.encoder = new edu.wpi.first.wpilibj.Encoder(port1, port2, false, edu.wpi.first.wpilibj.Encoder.EncodingType.k2X);
-    }
-    public double getRevs() {
-        return (isReversed ? -encoder.get() : encoder.get()) / 2048.0;
-    }
-    public void reset() {
-        encoder.reset();
+    CANCoder coder;
+    public Encoder(int canPort) {
+        this.coder = new CANCoder(canPort);
     }
 }
