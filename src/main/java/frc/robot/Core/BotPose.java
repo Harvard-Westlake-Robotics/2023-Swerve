86b866fa9148a14b0942ca69c62f2bad83bc419a
sammyRobotics <hwrobotics1@gmail.com>
2024-01-20 14:19:25 -0800

    Update BotPose.java

--- src/main/java/frc/robot/Core/BotPose.java
+++ src/main/java/frc/robot/Core/BotPose.java
@@ -1,50 +1,50 @@
-// import frc.robot.Core.Schedulable;
-// import frc.robot.Devices.LimeLight;
-// import frc.robot.Util.Vector2;
+import frc.robot.Core.Schedulable;
+import frc.robot.Devices.LimeLight;
+import frc.robot.Util.Vector2;
 
-// public class BotPose extends Schedulable {
-// private static double botPositionX, botPositionY, botPositionZ, pitch, yaw,
-// roll;
-// private static double retrievedLatency, tickTimeSinceEpoch;
+public class BotPose extends Schedulable {
+    private static double botPositionX, botPositionY, botPositionZ, pitch, yaw,
+            roll;
+    private static double retrievedLatency, tickTimeSinceEpoch;
 
-// public void start() {
+    public void start() {
 
-// }
+    }
 
-// public void tick(double dTime) {
-// botPositionX = LimeLight.getRobotX();
-// botPositionY = LimeLight.getRobotY();
-// botPositionZ = LimeLight.getRobotZ();
-// pitch = LimeLight.getRobotPitch();
-// yaw = LimeLight.getRobotYaw();
-// roll = LimeLight.getRobotRoll();
-// long currentTimeSinceEpoch = System.currentTimeMillis();
-// long receiveTimeSinceEpoch = LimeLight.getLastReceiveTime();
-// latency = LimeLight.getRobotLatency() + (currentTimeSinceEpoch -
-// receiveTimeSinceEpoch);
-// tickTimeSinceEpoch = currentTimeSinceEpoch;
-// }
+    public void tick(double dTime) {
+        botPositionX = LimeLight.getRobotX();
+        botPositionY = LimeLight.getRobotY();
+        botPositionZ = LimeLight.getRobotZ();
+        pitch = LimeLight.getRobotPitch();
+        yaw = LimeLight.getRobotYaw();
+        roll = LimeLight.getRobotRoll();
+        long currentTimeSinceEpoch = System.currentTimeMillis();
+        long receiveTimeSinceEpoch = LimeLight.getLastReceiveTime();
+        latency = LimeLight.getRobotLatency() + (currentTimeSinceEpoch -
+                receiveTimeSinceEpoch);
+        tickTimeSinceEpoch = currentTimeSinceEpoch;
+    }
 
-// public static Vector2 predictPosition(Vector2 velocity) {
-// long currentTimeSinceEpoch = System.currentTimeMillis();
-// long dataUsedLatency = currentTimeSinceEpoch - tickTimeSinceEpoch;
-// double finalLatency = latency + dataUsedLatency;
-// Vector2 predictedPositionChange = velocity.multiply(finalLatency);
-// Vector2 predictedPosition = new Vector2(botPositionX,
-// botPositionY).add(predictedPositionChange);
-// return predictedPosition;
-// }
+    public static Vector2 predictPosition(Vector2 velocity) {
+        long currentTimeSinceEpoch = System.currentTimeMillis();
+        long dataUsedLatency = currentTimeSinceEpoch - tickTimeSinceEpoch;
+        double finalLatency = latency + dataUsedLatency;
+        Vector2 predictedPositionChange = velocity.multiply(finalLatency);
+        Vector2 predictedPosition = new Vector2(botPositionX,
+                botPositionY).add(predictedPositionChange);
+        return predictedPosition;
+    }
 
-// public static double predictYaw(double angularVelocity) {
-// long currentTimeSinceEpoch = System.currentTimeMillis();
-// long dataUsedLatency = currentTimeSinceEpoch - tickTimeSinceEpoch;
-// double finalLatency = latency + dataUsedLatency;
-// double predictedAngleChange = angularVelocity * finalLatency;
-// double predictedAngle = yaw + predictedAngleChange;
-// return predictedAngle;
-// }
+    public static double predictYaw(double angularVelocity) {
+        long currentTimeSinceEpoch = System.currentTimeMillis();
+        long dataUsedLatency = currentTimeSinceEpoch - tickTimeSinceEpoch;
+        double finalLatency = latency + dataUsedLatency;
+        double predictedAngleChange = angularVelocity * finalLatency;
+        double predictedAngle = yaw + predictedAngleChange;
+        return predictedAngle;
+    }
 
-// public void end() {
+    public void end() {
 
-// }
-// }
+    }
+}
