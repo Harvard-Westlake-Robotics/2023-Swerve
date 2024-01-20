487934652fa77c9b98ebcd19828a52ead22a6634
1148HWRobotics <hwrobotics1@gmail.com>
2024-01-20 14:16:14 -0800

    Update BotPose.java
    Fix errors

--- src/main/java/frc/robot/Core/BotPose.java
+++ src/main/java/frc/robot/Core/BotPose.java
@@ -4,7 +4,8 @@ import frc.robot.Util.Vector2;
 
 public class BotPose extends Schedulable {
     private static double botPositionX, botPositionY, botPositionZ, pitch, yaw, roll;
-    private static double retrievedLatency, tickTimeSinceEpoch;
+    private static double retrievedLatency;
+    private static long tickTimeSinceEpoch;
 
     public void start() {
 
@@ -19,14 +20,14 @@ public class BotPose extends Schedulable {
         roll = LimeLight.getRobotRoll();
         long currentTimeSinceEpoch = System.currentTimeMillis();
         long receiveTimeSinceEpoch = LimeLight.getLastReceiveTime();
-        latency = LimeLight.getRobotLatency() + (currentTimeSinceEpoch - receiveTimeSinceEpoch);
+        retrievedLatency = LimeLight.getRobotLatency() + (currentTimeSinceEpoch - receiveTimeSinceEpoch);
         tickTimeSinceEpoch = currentTimeSinceEpoch;
     }
 
     public static Vector2 predictPosition(Vector2 velocity) {
         long currentTimeSinceEpoch = System.currentTimeMillis();
         long dataUsedLatency = currentTimeSinceEpoch - tickTimeSinceEpoch;
-        double finalLatency = latency + dataUsedLatency;
+        double finalLatency = retrievedLatency + dataUsedLatency;
         Vector2 predictedPositionChange = velocity.multiply(finalLatency);
         Vector2 predictedPosition = new Vector2(botPositionX, botPositionY).add(predictedPositionChange);
         return predictedPosition;
@@ -35,7 +36,7 @@ public class BotPose extends Schedulable {
     public static double predictYaw(double angularVelocity) {
         long currentTimeSinceEpoch = System.currentTimeMillis();
         long dataUsedLatency = currentTimeSinceEpoch - tickTimeSinceEpoch;
-        double finalLatency = latency + dataUsedLatency;
+        double finalLatency = retrievedLatency + dataUsedLatency;
         double predictedAngleChange = angularVelocity * finalLatency;
         double predictedAngle = yaw + predictedAngleChange;
         return predictedAngle;
